ff5e21ed7aee6d1a6dc3d223a28ba2facd0e0961
zhangjun <1922585388@qq.com>
2020-10-25 22:26:46 +0800

    .

---
+++ chapter4/MaxSunArray.h
@@ -0,0 +1,10 @@
+#include<vector>
+#include<pair>
+
+using namespace std;
+void MaxSumOfSunArray(vector<double> v){
+	for(auto it:v){
+		cout<<it<<endl;
+	}
+
+}
--- test.c
+++ test.c
@@ -1,7 +1,10 @@
 #include<iostream>
+#include"chapter4/MaxSunArray.h"
 
 using namespace std;
 
 int main(){
-	cout<<"pass test";
+	double arr[10]={1,2,3,4,5,6,7,8,9,0};
+	vector<double> v(arr,arr+10);
+	MaxSumOfSunArray(v);
 }
