0657fc17231b08dabd89392aca371aa0145271ff
zhangjun-ubuntu <1922585388@qq.com>
2020-11-22 23:31:06 +0800

    code and test MaxSubArray algorithm

--- chapter4/MaxSunArray.h
+++ chapter4/MaxSunArray.h
@@ -1,9 +1,26 @@
-#include<vector>
+//
+// Created by johnjun on 2020/11/22.
+//
 
-using namespace std;
-void MaxSumOfSunArray(vector<double> v){
-	for(auto it:v){
-		cout<<it<<endl;
-	}
+#ifndef INTRODUCT2ALGORITHM_MAXSUNARRAY_H
+#define INTRODUCT2ALGORITHM_MAXSUNARRAY_H
 
-}
+#include "vector"
+
+//all caculation include begin and end index
+class MaxSunArray {
+public:
+    MaxSunArray(const std::vector<double> &localArr);
+
+    std::pair<int, int> FindMaxSunArray();
+
+private:
+    std::vector<double> _localArr;
+
+    std::pair<std::pair<int, int>, double> FindMaxSunArray(int begin, int end);
+
+    std::pair<std::pair<int, int>, double> FindMaxSunArrayCrossMid(int begin, int end);
+};
+
+
+#endif //INTRODUCT2ALGORITHM_MAXSUNARRAY_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "chapter4/MaxSunArray.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double arr[] = {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
+    std::vector<double> v(arr, arr + 16);
+    MaxSunArray m(v);
+    auto result = m.FindMaxSunArray();
+    std::cout << result.first << '\t' << result.second;
     return 0;
 }
