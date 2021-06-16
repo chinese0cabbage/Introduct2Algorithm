babec0b98351845b50ef45bb52ff6a97b9401035
zhangjun01 <zhangjun01@cobotsys.com>
2021-06-16 17:45:25 +0800

    .

--- utils/heap_core.h
+++ utils/heap_core.h
@@ -14,6 +14,10 @@
 #define DEBUG_MODE
 #endif
 
+#ifdef DEBUG_MODE
+#include "debug_tool.h"
+#endif
+
 namespace I2A {
     /**
      * the core predicating method whether a range is heap or not
@@ -43,6 +47,7 @@ namespace I2A {
     /**
      * the core method to input an element to a heap
      * adjust element between __endIndex and __root,making the range meet the specification of heap
+     * 将__endIndex节点抬升到合适的位置
      * @tparam _RandomAccessIterator
      * @tparam _Distance
      * @tparam _Tp
@@ -53,34 +58,71 @@ namespace I2A {
      * @param __compare
      */
     template<typename _RandomAccessIterator, typename _Distance, typename _Tp, typename _Compare>
-    void __push_heap(_RandomAccessIterator __first, _Distance __root, _Distance __endIndex, _Tp __value,
-                     _Compare __compare) {
-        for (_Distance __parent = (__endIndex - 1) / 2;
-             __parent > __root && !__compare(__first + __parent, __endIndex);
-             __endIndex = __parent, __parent = (__endIndex - 1) / 2) {
-            *(__first + __parent) = std::move(*(__first + __endIndex));
+    void __push_heap(_RandomAccessIterator __first, _Distance __root, _Distance __endIndex, _Tp __value,_Compare __compare) {
+        _Distance __parent = (__endIndex - 1) / 2;//找到父节点
+        while (__endIndex > __root && __compare(__first + __parent, __value)) {
+            *(__first + __endIndex) = std::move(*(__first + __parent));//当前父节点向下沉降
+            __endIndex = __parent;
+            __parent = (__endIndex - 1) / 2;//向上遍历
         }
-        *(__first + __root) = std::move(__value);
+        *(__first + __parent) = std::move(__value);//比目标值小的元素都已经沉降，目标值填入空位
     }
 
+    /**
+     * 将root节点下降到合适的位置，下降方向为较小（大）的孩子子树方向
+     * @tparam _RandomAccessIterator
+     * @tparam _Distance
+     * @tparam _Tp
+     * @tparam _Compare
+     * @param __first
+     * @param __root
+     * @param __len
+     * @param __value
+     * @param __comp
+     */
     template<typename _RandomAccessIterator, typename _Distance, typename _Tp, typename _Compare>
-    void __adjust_heap(_RandomAccessIterator __first, _Distance __holeIndex, _Distance __len, _Tp __value, _Compare __comp) {
-        const _Distance __topIndex = __holeIndex;
-        _Distance __secondChild = __holeIndex;
+    void __adjust_heap(_RandomAccessIterator __first, _Distance __root, _Distance __len, _Tp __value, _Compare __comp) {
+        const _Distance __topIndex = __root;
+        _Distance __secondChild = __root;
         while (__secondChild < (__len - 1) / 2) {
             __secondChild = 2 * (__secondChild + 1);
             if (__comp(__first + __secondChild, __first + (__secondChild - 1)))
                 __secondChild--;
-            *(__first + __holeIndex) = _GLIBCXX_MOVE(*(__first + __secondChild));
-            __holeIndex = __secondChild;
+            *(__first + __root) = _GLIBCXX_MOVE(*(__first + __secondChild));
+            __root = __secondChild;
         }
         if ((__len & 1) == 0 && __secondChild == (__len - 2) / 2) {
             __secondChild = 2 * (__secondChild + 1);
-            *(__first + __holeIndex) = _GLIBCXX_MOVE(*(__first + (__secondChild - 1)));
-            __holeIndex = __secondChild - 1;
+            *(__first + __root) = _GLIBCXX_MOVE(*(__first + (__secondChild - 1)));
+            __root = __secondChild - 1;
         }
-        std::__push_heap(__first, __holeIndex, __topIndex, _GLIBCXX_MOVE(__value),
+        I2A::__push_heap(__first, __root, __topIndex, _GLIBCXX_MOVE(__value),
                          __gnu_cxx::__ops::__iter_comp_val(__comp));
+    }
+
+    /**
+     * 建堆采用了算法导论中将两个堆加一个元素合并成一个堆逐级向上合并以建堆的过程
+     * @tparam _RandomAccessIterator
+     * @tparam _Compare
+     * @param __first
+     * @param __last
+     * @param __comp
+     */
+    template<typename _RandomAccessIterator, typename _Compare>
+    void __make_heap(_RandomAccessIterator __first, _RandomAccessIterator __last, _Compare __comp) {
+        typedef typename std::iterator_traits<_RandomAccessIterator>::value_type _ValueType;
+        typedef typename std::iterator_traits<_RandomAccessIterator>::difference_type _DistanceType;
+        if (__last - __first < 2)
+            return;
+        const _DistanceType __len = __last - __first;
+        _DistanceType __parent = (__len - 2) / 2;
+        while (true) {
+            _ValueType __value = _GLIBCXX_MOVE(*(__first + __parent));
+            I2A::__adjust_heap(__first, __parent, __len, _GLIBCXX_MOVE(__value), __comp);
+            if (__parent == 0)
+                return;
+            __parent--;
+        }
     }
 }
 
