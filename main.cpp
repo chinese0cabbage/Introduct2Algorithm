bdbb1586d56b7a1dafd22e9677cb053513b647a8
zhangjun01 <zhangjun01@cobotsys.com>
2021-06-17 17:41:29 +0800

    .

--- main.cpp
+++ main.cpp
@@ -29,18 +29,6 @@ bool compare(double a, double b) {
 int main() {
     double arr[] = {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
     std::vector<double> v(arr, arr + 16);
-//    I2A::__adjust_heap(v.begin(),0,10,13,__gnu_cxx::__ops::__iter_less_iter());
-    I2A::__make_heap(v.begin(), v.end(), __gnu_cxx::__ops::__iter_less_iter());
-//    std::for_each(v.begin(), v.end(), [=](double i) { std::cout << i << "\t"; });
-//    std::cout << "\n";
-//    std::__adjust_heap(v.begin(),0,16,21,__gnu_cxx::__ops::__iter_less_iter());
-//    int i=0;
-//    while (i++<16) {
-//        std::__pop_heap(v.begin(), v.end()-i, v.end()-i, __gnu_cxx::__ops::__iter_less_iter());
-//        std::for_each(v.begin(), v.end(), [=](double i) { std::cout << i << "\t"; });
-//        std::cout << "\n";
-//    }
-//    std::__adjust_heap(v.begin(), 0, 16, 20, __gnu_cxx::__ops::__iter_less_iter());
     std::cout << "\n";
     std::for_each(v.begin(), v.end(), [=](double i) { std::cout << i << "\t"; });
     return 0;
